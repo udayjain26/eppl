812216f2575bf432070ce07353b3fc3d5da596ad
udayjain26 <uj9.uday@gmail.com>
2024-04-27 19:59:58 +0530

    TW Merge and Clsx prep

--- src/app/_components/page-wrapper.tsx
+++ src/app/_components/page-wrapper.tsx
@@ -4,7 +4,7 @@ export default function PageWrapper({
   children: React.ReactNode
 }) {
   return (
-    <div className="fixed left-16 top-16 flex h-full w-full max-w-full flex-col overflow-y-scroll text-5xl ">
+    <div className="fixed left-16 top-16 -m-2 flex h-full w-full max-w-full flex-col overflow-y-scroll rounded-3xl bg-white p-2">
       {children}
     </div>
   )
