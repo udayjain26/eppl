f9d425990f051fcdc51cde7017b74b35ff814ae1
udayjain26 <uj9.uday@gmail.com>
2024-05-03 12:18:19 +0530

    expanding the schema file

--- src/app/clients/page.tsx
+++ src/app/clients/page.tsx
@@ -1,10 +1,30 @@
+import Link from 'next/link'
 import PageWrapper from '../_components/page-wrapper'
-import CreateClientForm from './_components/create-form'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
 
-export default function ClientsPage() {
+export default function ClientsDashboard() {
   return (
     <PageWrapper>
-      <CreateClientForm />
+      <div>Client Dashboard</div>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </PageWrapper>
   )
 }
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -5,55 +5,13 @@ import {
   timestamp,
   index,
   pgEnum,
+  boolean,
 } from 'drizzle-orm/pg-core'
+import { stateEnum } from './enums'
+
+export { stateEnum } from './enums'
 
 export const createTable = pgTableCreator((name) => `eppl_${name}`)
-
-// ENUMS used in the application
-export const stateEnum = pgEnum('states', [
-  'Andhra Pradesh',
-  'Arunachal Pradesh',
-  'Assam',
-  'Bihar',
-  'Chhattisgarh',
-  'Goa',
-  'Gujarat',
-  'Haryana',
-  'Himachal Pradesh',
-  'Jharkhand',
-  'Karnataka',
-  'Kerala',
-  'Madhya Pradesh',
-  'Maharashtra',
-  'Manipur',
-  'Meghalaya',
-  'Mizoram',
-  'Nagaland',
-  'Odisha',
-  'Punjab',
-  'Rajasthan',
-  'Sikkim',
-  'Tamil Nadu',
-  'Telangana',
-  'Tripura',
-  'Uttar Pradesh',
-  'Uttarakhand',
-  'West Bengal',
-  'Andaman and Nicobar Islands',
-  'Chandigarh',
-  'Dadra and Nagar Haveli',
-  'Daman and Diu',
-  'Jammu and Kashmir',
-  'Lakshadweep',
-  'Delhi',
-  'Puducherry',
-  'Ladakh',
-])
-
-export const clientCreationStatus = pgEnum('client_creation_status', [
-  'New',
-  'Old',
-])
 
 export const clients = createTable(
   'clients',
@@ -64,9 +22,12 @@ export const clients = createTable(
     createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updated_at'),
     gstin: varchar('gstin', { length: 15 }),
-    clientCreationStatus: clientCreationStatus(
-      'client_creation_status',
-    ).notNull(),
+    isNewClient: boolean('is_new_client').default(false),
+    clientAddressLine1: varchar('client_address_line1', { length: 256 }),
+    clientAddressLine2: varchar('client_address_line2', { length: 256 }),
+    clientAddressCity: varchar('client_address_city', { length: 256 }),
+    clientAddressState: stateEnum('client_address_state'),
+    clientAddressPincode: varchar('client_address_pincode', { length: 6 }),
   },
   (table) => {
     return {
